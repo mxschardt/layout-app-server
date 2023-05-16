dfe43d3576722c91fbf226682039edda0dc49ade
Maxim Schardt <mxschardt@gmail.com>
2023-05-16 13:40:28 +0300

    Model modifications

--- Models/Post.cs
+++ Models/Post.cs
@@ -5,8 +5,6 @@ public class Post
     public int Id { get; set; }
     public string Title { get; set; } = string.Empty;
     public string Body { get; set; } = string.Empty;
-    public DateTime CreationDate { get; set; }
-    public DateTime? EditDate { get; set; }
+    public DateTime CreationDate { get; set; } = DateTime.Now;
     public int UserId { get; set; }
-    public User User { get; set; } = null!;
 }
